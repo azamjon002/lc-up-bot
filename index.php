2ade2799da02f4a24e7285a890765043b469da03
azamjon002 <azamjonqochqorov002@gmail.com>
2023-09-14 13:08:30 +0500

    test

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ require_once 'replyBtn.php';
 
 
 $botToken = '5975116041:AAHecUKDBOSDuJuQvKCfS0aETpZ5Naqy0oI';
-// https://api.telegram.org/bot5975116041:AAHecUKDBOSDuJuQvKCfS0aETpZ5Naqy0oI/setWebhook?url=https://419d-188-113-206-179.ngrok-free.app/lc-up/index.php
+// https://api.telegram.org/bot5975116041:AAHecUKDBOSDuJuQvKCfS0aETpZ5Naqy0oI/setWebhook?url=https://f348-188-113-221-228.ngrok-free.app/lc-up/index.php
 
 /**
  * @var $bot \TelegramBot\Api\Client | \TelegramBot\Api\BotApi
