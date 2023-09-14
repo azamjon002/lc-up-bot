4f50875b77cecb06d8d882e0458b54d87e634766
azamjon002 <azamjonqochqorov002@gmail.com>
2023-09-14 14:05:50 +0500

    test

--- index.php
+++ index.php
@@ -4,10 +4,8 @@ require 'vendor/autoload.php';
 require_once 'database.php';
 require_once 'inlineBtn.php';
 require_once 'replyBtn.php';
+require_once 'config.php';
 
-
-$botToken = '5975116041:AAHecUKDBOSDuJuQvKCfS0aETpZ5Naqy0oI';
-// https://api.telegram.org/bot5975116041:AAHecUKDBOSDuJuQvKCfS0aETpZ5Naqy0oI/setWebhook?url=https://f348-188-113-221-228.ngrok-free.app/lc-up/index.php
 
 /**
  * @var $bot \TelegramBot\Api\Client | \TelegramBot\Api\BotApi
