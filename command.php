3a5bac62ee13a0a30a6cdb36d07cc03428e84ef3
azamjon002 <azamjonqochqorov002@gmail.com>
2023-09-16 15:52:11 +0500

    test

--- command.php
+++ command.php
@@ -14,6 +14,7 @@ use ($bot, $request_contact, $main_menu_btn)
          $bot->sendMessage($chat_id, "Siz botdan oldin ro'yhatdan o'tgansiz!",'HTML');
          $bot->sendMessage($chat_id, "O'zingizga kerakli menyulardan birini tanlang!",'HTML', false, null,  $main_menu_btn);
     }else{
+        unlink("session/$chat_id.txt");
         $bot->sendMessage($chat_id,"Assalomu alaykum $first_name. Ro'yhatdan o'tgan parolingiz orqali o'z kabinetingizga kiring", 'HTML');
         $bot->sendMessage($chat_id,"Telefon raqamni kiriting\nNa'muna: 998991234455", 'HTML',false,null, $request_contact);
     }
